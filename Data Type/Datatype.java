d6ffb6b257de13d480bf92149d3cbe4b91fe662f
ramachandran-github <119684183+ramachandran-github@users.noreply.github.com>
2022-12-05 07:49:59 +0530

    Add files via upload

---
+++ Data Type/Datatype.java
@@ -0,0 +1,47 @@
+package lesson1;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+	  //boolean example
+		boolean IoT=true;
+	System.out.println(IoT);
+	
+	
+	//byte example
+	byte range;
+	range=127;
+	System.out.println(range);
+	
+	
+	//short example
+	short range1;
+	range1=-127;
+	System.out.println(range1);
+	
+	
+	//int example
+	int range2=300000000;
+	System.out.println(range2);
+	
+	//double example
+	double number=99.9;
+	System.out.println(number);
+	
+	//float example
+	float number1=99.9f;
+	
+	//char example (Unicode value of A is'/u0041')
+	char letter='\u0041';
+	System.out.println(letter);
+	char letter2=65; //ASCII value of'A'is 65
+	System.out.println(letter2);
+	 char letter3='A';
+	 
+	 System.out.println(letter3);
+	 
+
+	}
+
+}
+
